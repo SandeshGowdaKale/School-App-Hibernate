a82abbfef953d789344b91adce6bbe193c034f79
sandesh <sandeshgowda2021@gmail.com>
2022-09-26 17:07:47 +0530

    Done

---
+++ src/main/java/com/ty/school/dao/ReferencesBookDAO.java
@@ -0,0 +1,5 @@
+package com.ty.school.dao;
+
+public class ReferencesBookDAO {
+
+}
---
+++ src/main/java/com/ty/school/dto/Student.java
@@ -0,0 +1,42 @@
+package com.ty.school.dto;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Table(name = "student_details")
+@Entity
+public class Student {
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id
+	private int rollno;
+	private String name;
+	private double percentage;
+
+	public int getRollno() {
+		return rollno;
+	}
+
+	public void setRollno(int rollno) {
+		this.rollno = rollno;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPercentage() {
+		return percentage;
+	}
+
+	public void setPercentage(double percentage) {
+		this.percentage = percentage;
+	}
+
+}
